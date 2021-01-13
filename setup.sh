66c0c016cfc924e8176ef1dfdaa01fb322cfe954
Msksgm <4419517@ed.tus.ac.jp>
2021-01-13 09:34:26 +0900

    Fix

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=(.tmux-rename-session .tmux.conf .vimrc .zprezto .zlogin .zlogout .zprofile .zshenv .zshrc)
+DOT_FILES=(.tmux-rename-session .tmux.conf .vimrc .zinit .zlogin .zlogout .zprofile .zshenv .zshrc zsh-syntax-highlighting)
 
 for file in ${DOT_FILES[@]}; do
   if [ -e $HOME/$file ]; then
